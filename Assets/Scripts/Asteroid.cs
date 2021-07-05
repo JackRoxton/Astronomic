da0ac822d7250bf0afc95766b808fa3dd7bfb2e7
QuentinLamy <quentin.lamy16@gmail.com>
2021-07-05 17:03:14 +0200

    proto rapide

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float size;
+    void Start()
+    {
+        this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 300);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Asteroid asteroid = collision.gameObject.GetComponent<Asteroid>();
+        if(asteroid != null)
+        {
+            Destroy(asteroid.gameObject);
+            this.transform.localScale += Vector3.one * 0.4f;
+        }
+    }
+
+}
