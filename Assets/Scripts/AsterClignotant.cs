3e0ea25915f62d49e3724b2eaef19671f1f490e9
Isshinaru <arsene02031991@gmail.com>
2021-07-06 16:27:40 +0200

    Prefabs Asteroides

---
+++ Assets/Scripts/AsterClignotant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsterClignotant : MonoBehaviour
+{
+    private bool heavierThanPlayer;
+    [SerializeField]CharacterControler charControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heavierThanPlayer = true;
+        charControl = GameObject.Find("Player").GetComponent<CharacterControler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (heavierThanPlayer)
+        {
+            this.GetComponent<SpriteRenderer>().material.SetFloat("Vector1_EAFD24DB", Mathf.PingPong(2*Time.time, 1f));
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().material.SetFloat("Vector1_185AB46", Mathf.PingPong(2*Time.time,1f));
+        }
+    }
+}
